8974f20e081e841f9ac6935cbf5b3351cf93793c
Chad Huntley <chad@roadtripnation.com>
2016-12-20 08:58:30 -0800

    Added flags for custom port and path to check

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
-import "net/http"
-import "flag"
+import (
+	"net/http"
+	"flag"
+	"fmt"
+)
 
 func main() {
 	checkPath := flag.String("checkPath", "/var/www/letsencrypt", "The path to check")
 	serverPort := flag.String("port", "9009", "The server port")
+	flag.Parse()
 
+	fmt.Printf("Running server on port %s, checking the path %s", *serverPort, *checkPath)
 	panic(http.ListenAndServe(":" + *serverPort, http.FileServer(http.Dir(*checkPath))))
 }
